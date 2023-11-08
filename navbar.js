64e486f0bf94567e28206cbf7c14d42ba8bbd595
TesaTec <magnushende@gmail.com>
2023-11-08 21:19:13 +0100

    Changed so you open and close the navbar based on mouse location, instead of buttons

--- navbar.js
+++ navbar.js
@@ -1,3 +1,15 @@
+
+document.addEventListener("mousemove", updateDisplay, false);
+
+/*Checks whenever it should close or open the navbar */
+function updateDisplay(event) {
+    if(event.x <= 5) {
+        openNav();
+    } else if( event.x >= 251) {
+        closeNav();
+    }
+}
+
 /* Set the width of the side navigation to 250px and the left margin of the page content 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
